a3fcb8d02d49677115523c418f8d884e1eb55087
Christian Josephy <christian.josephy@baloise.ch>
2018-10-30 11:02:59 +0100

    Minor test change

--- src/main/java/org/open/insurance/qa/ui/taf/gw/App.java
+++ src/main/java/org/open/insurance/qa/ui/taf/gw/App.java
@@ -24,6 +24,6 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World2!");
     }
 }
