5a583a5a08b1aed2e2d780c718db4d8844401b1c
Christian Josephy <christian.josephy@baloise.ch>
2018-11-14 10:24:06 +0100

    Added GWButton - click only if enabled

---
+++ src/main/java/org/open/insurance/baloise/qa/ui/taf/gw/elements/GWButton.java
@@ -0,0 +1,27 @@
+package org.open.insurance.baloise.qa.ui.taf.gw.elements;
+
+import org.junit.Assert;
+
+import com.baloise.testautomation.taf.browser.elements.BrButton;
+
+public class GWButton extends BrButton {
+
+  private void waitUntilEnabled() {
+    Long time = System.currentTimeMillis();
+    while (isDisabled()) {
+      if (System.currentTimeMillis() > time + 60000L) {
+        Assert.fail("Button ist not enabled since more than one minute");
+      }
+    }
+  }
+
+  public boolean isDisabled() {
+    return find().getAttribute("class").contains("x-btn-disabled");
+  }
+
+  @Override
+  public void click() {
+    waitUntilEnabled();
+    super.click();
+  }
+}
