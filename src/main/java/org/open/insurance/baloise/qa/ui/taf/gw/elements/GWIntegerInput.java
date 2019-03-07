9030ce4180b1297487587da8250ce81e2666deaf
Christian Josephy <christian.josephy@baloise.ch>
2019-03-07 08:00:34 +0100

    Added GWIntegerInput with custom-commands

---
+++ src/main/java/org/open/insurance/baloise/qa/ui/taf/gw/elements/GWIntegerInput.java
@@ -0,0 +1,37 @@
+package org.open.insurance.baloise.qa.ui.taf.gw.elements;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+
+import com.baloise.testautomation.taf.browser.elements.BrIntegerInput;
+
+public class GWIntegerInput extends BrIntegerInput {
+
+  @Override
+  public void fillCustom() {
+    String action = fillValue.getCustom();
+    if (action.equals("{notvisible}")) {
+      Long timeoutInMsecs = component.getBrowserFinder().getTimeoutInMsecs();
+      try {
+        component.getBrowserFinder().setTimeoutInMsecs(200L);
+        find();
+      }
+      catch (Throwable e) {
+        return;
+      }
+      finally {
+        component.getBrowserFinder().setTimeoutInMsecs(timeoutInMsecs);
+      }
+      Assert.fail("element was found but should NOT: " + name);
+    }
+    if (action.startsWith("{isreadonly}")) {
+      String value = action.replace("{isreadonly}", "");
+      WebElement element = find();
+      Assert.assertTrue("Element is not 'readonly', but it should be", "div".equalsIgnoreCase(element.getTagName()));
+      Assert.assertEquals("Text does not match", value, element.getText());
+      return;
+    }
+    Assert.fail("custom action not supported yet: " + action);
+  }
+
+}
